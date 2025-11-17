358034bcefd391989d6fedf5986985eda4fe4d1c
Miangaly Rafiringa <miangalyrafi@gmail.com>
2025-11-17 20:17:30 +0100

    ajout page d'accueil, navbar/footer

---
+++ front/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+
+const routes: Routes = [
+  { path: '', component: AccueilComponent }, 
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
