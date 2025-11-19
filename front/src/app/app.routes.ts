7c105c7d51d00c285884d6c9fc4668116a8beca5
Miangaly Rafiringa <miangalyrafi@gmail.com>
2025-11-19 13:39:34 +0100

    ajout de la catégories

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { ProduitSeulComponent } from './pages/produit-seul/produit-seul.component';
 import { AproposComponent } from './pages/apropos/apropos.component';
 import { PanierComponent } from './pages/panier/panier.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
 export const routes: Routes = [
      { path: '', component: AccueilComponent },
@@ -17,6 +18,7 @@ export const routes: Routes = [
      { path: 'produit-seul/:id', component: ProduitSeulComponent },
      { path: 'apropos', component: AproposComponent },
      { path: 'panier', component: PanierComponent },
+     { path: 'categories', component: CategoriesComponent },
 
 
 ];
